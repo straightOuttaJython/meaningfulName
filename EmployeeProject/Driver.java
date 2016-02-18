15ff51658f677b8cde3bf8f4e5263c26b1a44b38
Alexander Seefeldt <atseefeldt@linpe238vm5.csbsju.edu>
2016-02-18 15:35:09 -0600

    Terminal 1 in Step 3: Modified Driver.java

--- EmployeeProject/Driver.java
+++ EmployeeProject/Driver.java
@@ -11,9 +11,6 @@ public class Driver
       // edu.csbsju.cs.Employee here
       Employee harry = new Employee("Harry Hacker", 50000, 1989, 10, 1);
 
-      // raise salary by 5%
-      harry.raiseSalary(5);
-
       // print out information about harry
       // use java.lang.System.out here
       out.println("name=" + harry.getName() + ",salary=" + harry.getSalary());
