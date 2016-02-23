8dc1478927b20431925ff90a00cee81bd4de17d6
Alexander Seefeldt <atseefeldt@nx.csbsju.edu>
2016-02-23 14:44:56 -0600

    Terminal 1 in Step 4: Modified Driver.java

--- EmployeeProject/Driver.java
+++ EmployeeProject/Driver.java
@@ -10,7 +10,7 @@ public class Driver
       // because of the import statement, we don't have to use
       // edu.csbsju.cs.Employee here
       Employee harry = new Employee("Harry Hacker", 50000, 1989, 10, 1);
-
+      harry.raiseSalary(10);
       // print out information about harry
       // use java.lang.System.out here
       out.println("name=" + harry.getName() + ",salary=" + harry.getSalary());
